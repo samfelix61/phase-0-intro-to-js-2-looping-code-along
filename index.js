7aded6bf3dda2d277eae232178069c553d8681f9
samfelix61 <samfelix61@gmail.com>
2024-03-27 07:48:42 +0300

    complete assmnt

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+let newArray = [];
+
+
+function writeCards(arr, event){
+  
+    for(let i = 0; i < arr.length; i++){
+      newArray.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+    }
+  return newArray;
+  
+}
+
+
+function countDown(){
+    let i = 0;
+    while(i <= 10){
+        console.log(i)
+        i++;
+    }
+}
